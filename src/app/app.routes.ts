5cd5b3cf6badfe91f5deef8fd4867c46900bc96e
jesu-m0 <jesusmd2000@gmail.com>
2025-03-29 17:57:05 +0100

    Routes and page components created.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: ListPageComponent },
+      { path: 'details/:id', component: DetailsPageComponent },
+      { path: '**', redirectTo: 'list' }
+];

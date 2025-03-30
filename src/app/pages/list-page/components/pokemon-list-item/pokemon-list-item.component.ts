531c11a974da19a9d4ab913d046cc27ae2c688ab
jesu-m0 <jesusmd2000@gmail.com>
2025-03-30 14:42:46 +0200

    List item component done.

--- src/app/pages/list-page/components/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/pages/list-page/components/pokemon-list-item/pokemon-list-item.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonListItem } from '../../../../core/pokedev.model';
 
 @Component({
   selector: 'app-pokemon-list-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokemon-list-item.component.html',
   styleUrl: './pokemon-list-item.component.css'
 })
 export class PokemonListItemComponent {
-
+      @Input() pokemon!: PokemonListItem;
 }
--- src/app/pages/list-page/list-page.component.ts
+++ src/app/pages/list-page/list-page.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeDevService } from '../../core/pokedev.service';
+import { PokemonListItem } from '../../core/pokedev.model';
+import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon-list-item.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-page',
-  imports: [],
+  imports: [CommonModule, PokemonListItemComponent],
   templateUrl: './list-page.component.html',
   styleUrl: './list-page.component.css'
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+      pokemons: PokemonListItem[] = [];
+
+      constructor(private pokeService: PokeDevService) {}
+
+      ngOnInit(): void {
+
+            this.pokeService.getPokemonList().subscribe({
+                  next: (pokemons: PokemonListItem[]) => {
+                        this.pokemons = pokemons;
+                  },
+                  error: (err) => {
+                        console.error("Error while getting pokemon list: ", err);
+                  }
+            })
+
+      }
 
 }
